6a9b4cddf2b4374ad1b0ad151a764e7da59b26ec
Anderson Machado <anderson.machadop@gmail.com>
2024-05-03 22:22:50 -0300

    Criando as classes based view

--- djangoapp/blog/urls.py
+++ djangoapp/blog/urls.py
@@ -1,13 +1,18 @@
-from blog.views import index, page, post, created_by, category, tag, search
+from blog.views import (CreatedByListView, PostListView, page, post,  
+                        category, tag, search)
 from django.urls import path
 
 app_name = 'blog'
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', PostListView.as_view(), name='index'),
     path('post/<slug:slug>/', post, name='post'),
     path('page/<slug:slug>/', page, name='page'),
-    path('created_by/<int:author_pk>/', created_by, name='created_by'),
+    path(
+        'created_by/<int:author_pk>/',
+        CreatedByListView.as_view(),
+        name='created_by'
+    ),
     path('category/<slug:slug>/', category, name='category'),
     path('tag/<slug:slug>/', tag, name='tag'),
     path('search/', search, name='search'),
